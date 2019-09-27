1a8f76e5c17c434a294486f4c8d4fbcf5b734137
Andrey Chertkov <andreychertckov@gmail.com>
2019-09-27 18:25:37 +0300

    Structure

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import SandBox from './src/components/SandBox';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <SandBox></SandBox>
     </View>
   );
 }
---
+++ src/components/SandBox.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function SandBox() {
+    return (
+        <p>Sand Box</p>
+    )
+}
