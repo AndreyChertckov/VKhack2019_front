01d6ad1c8df58e4fa78077fa2341f5f307f17ff5
Nagellan <31626879+Nagellan@users.noreply.github.com>
2019-09-28 14:34:33 +0300

    Complete clock functions * Fixed bugs, corrected logics.

--- src/components/ClockView.js
+++ src/components/ClockView.js
@@ -6,45 +6,75 @@ export default function ClockView(props) {
     const radius = "110",
         circumference = +radius * 2 * Math.PI,
         strokeDasharray = circumference,
-        [posStrokeDashoffset, setPosStrokeDashoffset] = useState(circumference),
-        [negStrokeDashoffset, setNegStrokeDashoffset] = useState(circumference),
         [posMins, setPosMins] = useState(0),
-        [negMins, setNegMins] = useState(0);
+        [negMins, setNegMins] = useState(0),
+        [clockFaceRotation, setClockFaceRotation] = useState("-90deg"),
+        [offsetPositive, setOffsetPositive] = useState(circumference),
+        [offsetNegative, setOffsetNegative] = useState(circumference),
+        [bottomLimit, setBottomLimit] = useState(0), 
+        [topLimit, setTopLimit] = useState(1440), 
+        [hourLimit, setHourLimit] = useState(60);
 
     function addProgress(mins) {
-        if (posMins < 1440) {
-            const offset = circumference - (posMins + mins) / 60 * circumference;
-            setPosMins(posMins + mins);
-            if (posMins < 60)
-                setPosStrokeDashoffset(offset);
-            if (posMins == negMins) {
-                setNegMins(posMins + mins);
-                if (posMins < 60)
-                    setNegStrokeDashoffset(offset);
+        if (posMins < topLimit) {
+            if (posMins >= hourLimit)
+                setClockFaceRotation((+clockFaceRotation.slice(0, -3) + mins*6) + "deg");
+            else {
+                if (posMins >= negMins) {
+                    setOffsetNegative(offsetNegative - mins/60*circumference);
+                    setOffsetPositive(offsetPositive - mins/60*circumference);
+                } else
+                    setOffsetNegative(offsetNegative - mins/60*circumference);
             }
+
+            setPosMins(posMins + mins);
+            if (posMins >= negMins)
+                setNegMins(posMins + mins);
         }
     }
 
     function removeProgress(mins) {
-        if (posMins > 0) {
-            const offset = circumference - (posMins - mins) / 60 * circumference;
+        if (posMins > bottomLimit) {
+            if (negMins >= hourLimit && posMins <= negMins - 60) {
+                setClockFaceRotation((+clockFaceRotation.slice(0, -3) - mins*6) + "deg");
+            } else
+                setOffsetNegative(offsetNegative + mins/60*circumference);
             setPosMins(posMins - mins);
-            setPosStrokeDashoffset(offset)
         }
     }
     
-    function resetProgress() {
+    function reset() {
+        setClockFaceRotation("-90deg");
         setPosMins(0);
         setNegMins(0);
-        setPosStrokeDashoffset(circumference);
-        setNegStrokeDashoffset(circumference);
+        setBottomLimit(0);
+        setTopLimit(1440);
+        setHourLimit(60);
+        setOffsetPositive(circumference);
+        setOffsetNegative(circumference);
+    }
+
+    function resetProgress() {
+        setNegMins(posMins);
+        setBottomLimit(posMins);
+        setTopLimit(topLimit + posMins);
+        setHourLimit(hourLimit + posMins);
+        setClockFaceRotation((-90 + posMins*6) + "deg");
+        setOffsetPositive(circumference);
+        setOffsetNegative(circumference);
     }
 
     const screenWidth = Math.round(Dimensions.get('window').width);
 
     return (
         <React.Fragment>
-            <Svg style={styles.rot} className="prefix__progress-ring" width={screenWidth} height={screenWidth} {...props}>
+            <Svg 
+                style={{transform: [{rotateZ: clockFaceRotation}]}}
+                className="prefix__progress-ring" 
+                width={screenWidth} 
+                height={screenWidth} 
+                {...props}
+            >
                 <Circle
                     stroke="#0e53a7"
                     strokeWidth="20"
@@ -56,7 +86,7 @@ export default function ClockView(props) {
                 <Circle
                     stroke="#00a876"
                     strokeDasharray={strokeDasharray}
-                    strokeDashoffset={negStrokeDashoffset}
+                    strokeDashoffset={offsetPositive}
                     strokeWidth="20"
                     fill="transparent"
                     r={radius}
@@ -66,13 +96,20 @@ export default function ClockView(props) {
                 <Circle
                     stroke="#f83e5b"
                     strokeDasharray={strokeDasharray}
-                    strokeDashoffset={posStrokeDashoffset}
+                    strokeDashoffset={offsetNegative}
                     strokeWidth="20"
                     fill="transparent"
                     r={radius}
                     cx="50%"
                     cy="50%"
                 />
+            </Svg>
+            <Svg
+                style={{position: "absolute", transform: [{rotateZ: "-90deg"}]}}
+                width={screenWidth} 
+                height={screenWidth} 
+                {...props}
+            >
                 {/* Minute hand */}
                 <Rect
                     transform={"rotate("+ posMins*6 +", " + (screenWidth/2) + ", " + (screenWidth/2) + ")"}
@@ -107,13 +144,12 @@ export default function ClockView(props) {
             </Svg>
             <Button onPress={() => addProgress(5)} title="ADD" />
             <Button onPress={() => removeProgress(5)} title="REMOVE" />
-            <Button onPress={() => resetProgress()} title="RESET" />
+            <Button onPress={() => resetProgress()} title="RESET PROGRESS" />
+            <Button onPress={() => reset()} title="RESET" />
         </React.Fragment>
     )
 }
 
 const styles = StyleSheet.create({
-    rot: {
-        transform: [{rotateZ: "-90deg"}]
-    }
+    
 });
